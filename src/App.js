f4b58014c14ec6a1d9acef4e7c814293bbc4f78f
Yuri <yuri.tayama@linksports.co.jp>
2019-05-20 00:00:57 +0900

    add CreateProject component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/NavBar'
 import Dashboard from './components/dashboard/DashBoard'
 import ProjectDetails from './components/projects/ProjectDetails'
 import SignIn from './components/auth/SignIn'
 import SignUp from './components/auth/SignUp'
+import CreateProject from './components/projects/CreateProject'
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={Dashboard} />
-          <Route path='/project/:id' component={ProjectDetails} />
-          <Route path='/signin' component={SignIn} />
-          <Route path='/signup' component={SignUp} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Dashboard} />
+            <Route path='/project/:id' component={ProjectDetails} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
+            <Route path='/create' component={CreateProject} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
