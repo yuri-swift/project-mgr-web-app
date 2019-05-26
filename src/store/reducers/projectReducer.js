1a481ffeffd9b1f2d8fc2d3a26d8dda16155d4d4
Yuri <yuri.tayama@linksports.co.jp>
2019-05-26 21:26:35 +0900

    add redux & make reducer

---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,13 @@
+const initState ={
+  projects: [
+    {id: '1', title: '1xxxx xx xxxx', content: 'xxx xxxxx'},
+    {id: '2', title: '2xxxx xx xxxx', content: 'xxx xxxxx'},
+    {id: '3', title: '3xxxx xx xxxx', content: 'xxx xxxxx'},
+  ]
+}
+
+const projectReducer = (state = initState, action) => {
+  return state
+}
+
+export default projectReducer
