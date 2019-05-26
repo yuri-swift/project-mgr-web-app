27e6a2d48144c32cae4ac00a729ff5eb8a56dbd8
Yuri <yuri.tayama@linksports.co.jp>
2019-05-26 21:58:55 +0900

    add redux-thunk & add action of  create project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './store/reducers/rootReducer';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 serviceWorker.unregister();
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -7,6 +7,10 @@ const initState ={
 }
 
 const projectReducer = (state = initState, action) => {
+  switch(action.type) {
+    case 'CREATE_PROJECT':
+      console.log('careated project', action.project)
+  }
   return state
 }
 
