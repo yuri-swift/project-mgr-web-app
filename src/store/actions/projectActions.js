7b61d5d0853d8871800fad131b0b57f222993f4e
Yuri <yuri.tayama@linksports.co.jp>
2019-05-26 23:55:10 +0900

    add firebase & add POST method

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,17 @@
 export const createProject = (project) => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call to db 
-    dispatch({ type: 'CREATE_PROJECT', project });
+    const firestore = getFirestore();
+    firestore.collection('projects').add({
+      ...project,
+      authorFirstName: 'Yuri',
+      authorLastName: 'Hotate',
+      authorId: 12345,
+      createdAt: new Date()
+    }).then(() => {
+      dispatch({ type: 'CREATE_PROJECT', project });
+    }).catch((err) => {
+      dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+    })
   }
 };
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -10,8 +10,13 @@ const projectReducer = (state = initState, action) => {
   switch(action.type) {
     case 'CREATE_PROJECT':
       console.log('careated project', action.project)
+      return state;
+    case 'CREATE_PROJECT_ERROR':
+      console.log('careated project error', action.err)
+      return state;
+    default:
+      return state;
   }
-  return state
 }
 
 export default projectReducer
