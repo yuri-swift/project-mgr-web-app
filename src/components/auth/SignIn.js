aae1bf2f9de6e1e8b5bd6807642a3497c34976d8
Yuri <yuri.tayama@linksports.co.jp>
2019-06-01 22:37:08 +0900

    signin with firebase authentication

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { signIn } from '../../store/actions/authActions'
 
 class SignIn extends Component {
   state = {
     email: '',
-    password: '',
+    password: ''
   }
   handleChange = (e) => {
     this.setState({
@@ -11,36 +13,45 @@ class SignIn extends Component {
     })
   }
   handleSubmit = (e) => {
-    // デフォルトのアクション(例:フォームであればデータの送信)をキャンセルする
     e.preventDefault();
-    console.log(this.state)
+    this.props.signIn(this.state)
   }
-
   render() {
+    const { authError } = this.props;
     return (
       <div className="container">
-        <form onSubmit={this.handleSubmit} className="white">
-
+        <form className="white" onSubmit={this.handleSubmit}>
           <h5 className="grey-text text-darken-3">Sign In</h5>
-
-          <div className="inpu-field">
+          <div className="input-field">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" onChange={this.handleChange} />
+            <input type="email" id='email' onChange={this.handleChange} />
           </div>
-
-          <div className="inpu-field">
+          <div className="input-field">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" onChange={this.handleChange} />
+            <input type="password" id='password' onChange={this.handleChange} />
           </div>
-
           <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0">Login</button>
+            <div className="center red-text">
+              { authError ? <p>{authError}</p> : null }
+            </div>
           </div>
-
         </form>
       </div>
     )
   }
 }
 
-export default SignIn
+const mapStateToProps = (state) => {
+  return{
+    authError: state.auth.authError
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signIn: (creds) => dispatch(signIn(creds))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks'
+import { connect } from 'react-redux'
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const { auth } = props;
+
   return (
     <nav className= "nav-wrapper grey darken-3">
       <div className="container">
@@ -15,4 +18,10 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+const mapStateToProps = (state) => {
+  return{
+    auth: state.firebase.auth
+  }
+}
+
+export default connect(mapStateToProps)(Navbar)
